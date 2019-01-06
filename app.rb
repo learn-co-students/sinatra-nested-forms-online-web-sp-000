d0edd0ca5e815f5415334719d80fe6d2f0ad0118
Cheng Vue <cvu3@ucsc.edu>
2019-01-06 01:57:53 +0000

    Done.

--- app.rb
+++ app.rb
@@ -4,6 +4,22 @@ module FormsLab
   class App < Sinatra::Base
 
     # code other routes/actions here
+    get "/" do
+      "Welcome to the Nested Forms Lab! let's navigate to the '/new'"
+    end
+
+    get "/new" do
+      erb :'pirates/new'
+    end
+
+    post "/pirates" do
+      params["pirate"]["ships"].each {|ship| Ship.new(ship)}
+      @ship1 = Ship.all[0]
+      @ship2 = Ship.all[1]
+      @pirate = Pirate.new(params["pirate"])
+
+      erb :'pirates/show'
+    end
 
   end
 end
