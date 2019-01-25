78fd65911d79e728952af4dba50054dacd1304e7
Joe Ganobsik <jganobsik@gmail.com>
2019-01-25 19:57:37 +0000

    Done.

--- Gemfile
+++ Gemfile
@@ -1,7 +1,12 @@
 source "https://rubygems.org"
 
+gem 'activerecord', '4.2.5', :require => 'active_record'
+gem 'sinatra-activerecord', :require => 'sinatra/activerecord'
+
 gem 'sinatra', require: 'sinatra/base'
 gem 'sinatra-reloader', require: false
+gem 'sqlite3'
+gem 'rake'
 gem 'shotgun'
 gem 'pry'
 gem 'tux'
--- app.rb
+++ app.rb
@@ -4,6 +4,27 @@ module FormsLab
   class App < Sinatra::Base
 
     # code other routes/actions here
+    get '/pirates' do 
+      @pirates = Pirate.all 
+      erb :'pirates/index'
+   end
+  
+  post '/pirates' do
+      @pirate = Pirate.new(params[:pirate])
 
+      params[:pirate][:ships].each do |details|
+        Ship.new(details)
+      end
+      @ships = Ship.all
+
+      erb :'pirates/show'
+    end
+    
+  get '/' do
+    erb :root
+  end 
+  get '/new' do 
+    erb :'pirates/new'
   end
+end
 end
