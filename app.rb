f37c3db6677903faecf13fc4f6c7bc6398204a0d
Chris Dalla Santa <chris.dallasanta@gmail.com>
2019-04-25 17:57:16 +0000

    Done.

--- app.rb
+++ app.rb
@@ -4,6 +4,24 @@ module FormsLab
   class App < Sinatra::Base
 
     # code other routes/actions here
+    get '/' do
+      erb :root
+    end
 
+    get '/new' do
+      erb :"pirates/new"
+    end
+
+    post '/pirates' do
+      #make a new Pirate
+      @pirate = Pirate.new(params[:pirate])
+      #make new ships
+      params[:pirate][:ships].each do |ship_details|
+        Ship.new(ship_details)
+      end
+
+      #display the info the user just submitted
+      erb :'pirates/show'
+    end
   end
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,19 @@
 class Ship
+  attr_accessor :name, :type, :booty
+  @@ALL_SHIPS = []
+
+  def initialize(ship_params)
+    @name = ship_params[:name]
+    @type = ship_params[:type]
+    @booty = ship_params[:booty]
+    @@ALL_SHIPS << self
+  end
+
+  def self.all
+    @@ALL_SHIPS
+  end
+
+  def self.clear
+    @@ALL_SHIPS = []
+  end
 end
