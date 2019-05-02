ba87086daeca464c5c671b6e737c8c1a6c09381e
ch2isk4kos <ch2isk4kos@gmail.com>
2019-05-02 18:42:49 -0400

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,20 @@
 class Pirate
+    # Pirates should have a name, weight, and height.
+    # Write a class method that returns all the pirates.
+
+    attr_accessor :name, :weight, :height
+
+    PIRATES = []
+
+    def initialize(params)
+        @name = params[:name]
+        @weight = params[:weight]
+        @height = params[:height]
+
+        PIRATES << self
+    end
+
+    def self.all
+        PIRATES
+    end
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,25 @@
 class Ship
+     # The ship class has a name, type, and booty attributes.
+     # Write a class method .all that returns all the ships
+     # Write a class method .clear that deletes all ships.
+
+     attr_accessor :name, :type, :booty
+
+     @@all = []
+
+     def initialize(args)
+         @name = args[:name]
+         @type = args[:type]
+         @booty = args[:booty]
+
+         @@all << self
+     end
+
+     def self.all
+         @@all
+     end
+
+     def self.clear
+         @@all
+     end
 end
