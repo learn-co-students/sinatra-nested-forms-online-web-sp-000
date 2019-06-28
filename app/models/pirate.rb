93eb8ae44c4f4aec0c685e4f917f5f4c71e32096
Yingqi Chen <chanwk18@hotmail.com>
2019-06-28 07:18:53 +0000

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,17 @@
 class Pirate
+  attr_accessor :name, :weight, :height
+
+  PIRATE=[]
+
+  def initialize(name,weight,height)
+    @name=name
+    @weight=weight
+    @height=height
+    PIRATE<<self
+  end
+
+  def self.all
+    PIRATE
+  end
+
 end
