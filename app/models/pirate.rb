ef333eb75607eca2baca982a3a2c32abbcb0e01d
Ramon Chavez <rachavez1974@gmail.com>
2019-07-11 01:44:04 -0400

    Add Pirate model

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,20 @@
 class Pirate
+  attr_accessor :name, :weight, :height
+  @@all = []
+
+  def initialize(pirate_attributes)
+    pirate_attributes.each { |key, value| self.send(("#{key}="), value) }
+
+    save << self
+  end
+
+  def self.all
+    @all
+  end
+
+  def save
+    self.class.all << self
+  end
+
 end
+
