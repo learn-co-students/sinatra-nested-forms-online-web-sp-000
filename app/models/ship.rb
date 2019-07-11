7dd5742449b9ac51e42a7dd3c1928590bf2c8174
Ramon Chavez <rachavez1974@gmail.com>
2019-07-11 01:50:12 -0400

    Add Ship model

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,3 +1,20 @@
 class Ship
-  
+  attr_accessor :name, :type, :booty
+  @@all = []
+
+  def initialize(ship_attributes)
+    ship_attributes.each { |key, value| self.send(("#{key}="), value) }
+  end
+
+  def save
+    self.class.all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.clear
+    @@all.clear
+  end
 end
