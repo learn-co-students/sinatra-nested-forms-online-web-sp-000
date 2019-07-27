4484d42f1cd8a1575013e75eb3e6661ae2654081
Meisuleen Kased <meisuleen@gmail.com>
2019-07-27 18:45:58 +0000

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,15 @@
 class Pirate
+  attr_accessor :name, :weight, :height
+
+  pir_array = []
+
+  def initialize(args)
+    @name = args[:name]
+    @weight = args[:weight]
+    @height = args[:height]
+  end
+
+  def self.all
+    pir_array
+  end
 end
