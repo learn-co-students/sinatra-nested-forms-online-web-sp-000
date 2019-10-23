c3e50f53be9463e0a960db678c71408550482600
thisisSDK <25039980+thisisSDK@users.noreply.github.com>
2019-10-23 16:27:45 -0400

    fixes pirate.rb

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -2,11 +2,16 @@ class Pirate
 
   attr_accessor :name, :weight, :height
 
-  @@all = []
+  PIRATES = []
 
-  def initialize(name, weight, height)
-    @name = name
-    @weight = weight
-    @height = height
+  def initialize(args)
+    @name = args[:name]
+    @weight = args[:weight]
+    @height = args[:height]
+    PIRATES << self
+  end
+
+  def self.all
+    PIRATES
   end
 end
