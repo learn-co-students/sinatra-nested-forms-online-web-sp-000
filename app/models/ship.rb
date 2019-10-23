4907547e0f86a051bb2ad1ecb7a7f308b216ec9f
thisisSDK <25039980+thisisSDK@users.noreply.github.com>
2019-10-23 16:25:35 -0400

    mocks up models for pirates and ships.

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,21 @@
 class Ship
+
+  attr_accessor :name, :type, :booty
+
+  @@ships = []
+
+  def initialize(args)
+    @name = args[:name]
+    @type = args[:type]
+    @booty = args[:boooty]
+  end
+
+  def self.all
+    @@ships
+  end
+
+  def self.clear
+    @@ships
+  end
+
 end
