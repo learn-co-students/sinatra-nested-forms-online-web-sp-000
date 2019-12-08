37affd93bde2e250628cee0768ee20b387b74b1e
Christopher Dent <christopherdent01@gmail.com>
2019-12-08 22:33:49 +0000

    Done.

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,25 @@
 class Ship
+  attr_accessor :name, :type, :booty, :pirate
+  
+
+SHIPS = [] 
+  
+  def initialize(args)
+    @name = args[:name]
+    @type = args[:type]
+    @booty = args[:booty]
+    SHIPS << self
+    
+  end 
+  
+  
+  def self.all
+    SHIPS
+  end 
+  
+  def self.clear 
+    SHIPS
+  end 
+  
+  
 end
