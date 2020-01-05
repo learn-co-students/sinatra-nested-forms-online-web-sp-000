f4dc74757d82f181161ac6afce75435c3ceec1b3
David Strauch <dmaster18@gmail.com>
2020-01-05 18:17:39 +0000

    Done.

--- app.rb
+++ app.rb
@@ -2,11 +2,30 @@ require './environment'
 
 module FormsLab
   class App < Sinatra::Base
-    get 'pirates/new' do 
+    get '/' do
+      erb :index
+    end
+    
+    get '/new' do 
       erb :new
     end
     
-    post '/'
-
+    post '/pirates' do 
+      @name = params['pirate[name]']
+      @weight = params['pirate[weight]']
+      @height = params['pirate[height]']
+      @ship1_name = params['pirate[ships][][name]'] 
+      @ship1_type = params['pirate[ships][][type]'] 
+      @ship1_booty = params['pirate[ships][][booty]'] 
+      @ship2_name = params['pirate[ships][][name]'] 
+      @ship2_type = params['pirate[ships][][type]'] 
+      @ship2_booty = params['pirate[ships][][booty]'] 
+      puts params
+      erb :show
+    end
+    
+    get '/pirates' do
+      erb :show
+    end
   end
 end
