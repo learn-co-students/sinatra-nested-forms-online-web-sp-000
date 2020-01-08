efe44ccc61a699a660674d3669f57ebc931f7939
David Strauch <dmaster18@gmail.com>
2020-01-08 15:57:21 +0000

    Done.

--- app.rb
+++ app.rb
@@ -17,7 +17,7 @@ module FormsLab
     end
     
     post '/pirates' do 
-      pirate = Pirate.new(name: params[:pirate][:name, height: params[:pirate][:height], weight: params[:pirate][:weight]])
+      pirate = Pirate.create(name: params[:pirate][:name, height: params[:pirate][:height], weight: params[:pirate][:weight]])
       params[:pirate][:ships].each do |ship_data|
         ship = Ship.new(ship_data)
         ship.pirate = pirate
