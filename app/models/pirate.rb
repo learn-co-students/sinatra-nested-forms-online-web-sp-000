02208b2f1c6288ee7e4e3d1c4d2b2a72ddd16847
Anthony Tilelli <anthony@tilelli.me>
2020-02-02 21:54:40 +0000

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,19 @@
 class Pirate
+  attr_accessor :name, :weight, :height
+  @@existing = []
+  
+  def initialize(name, weight, height)
+    @name = name
+    @weight = weight
+    @height = height
+    @@existing << self
+  end
+  
+  def self.all
+    @@existing
+  end
+  
+  def self.clear
+    @@existing.clear
+  end
 end
