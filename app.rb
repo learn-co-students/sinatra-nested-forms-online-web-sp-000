526b6fb6c4d4035df7d4cca7c5f1d1b615ee0402
Oliver Dahl <oliverpdahl@gmail.com>
2020-03-01 12:28:54 +0000

    Done.

--- app.rb
+++ app.rb
@@ -4,6 +4,22 @@ module FormsLab
   class App < Sinatra::Base
 
     # code other routes/actions here
+    get '/' do
+      erb :root
+    end
 
+    get '/new' do
+      erb :new
+    end
+
+    post '/pirates' do
+      p_params = params[:pirate]
+      s1_params = params[:pirate][:ships][0]
+      s2_params = params[:pirate][:ships][1]
+      @pirate = Pirate.new(p_params[:name],p_params[:height],p_params[:weight])
+      @ship_1 = Ship.new_from_hash(s1_params)
+      @ship_2 = Ship.new_from_hash(s2_params)
+      erb :show
+    end
   end
 end
