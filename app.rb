fe96a9af04a33d83657fb503fcf0010e1bd2f17b
Michael Carroll <carrollm2@gmail.com>
2020-03-03 04:27:28 +0000

    Done.

--- app.rb
+++ app.rb
@@ -12,11 +12,11 @@ module FormsLab
       erb :'pirates/new'
     end
 
-    post 'pirates/pirates' do
+    post '/pirates' do
       @pirate = Pirate.new(params[:pirate_name], params[:pirate_weight], params[:pirate_height])
       @ship_1 = Ship.new(params[:ship_name_1], params[:ship_type_1], params[:ship_booty_1])
       @ship_2 = Ship.new(params[:ship_name_2], params[:ship_type_2], params[:ship_booty_2])
-      erb :show
+      erb :'pirates/show'
     end
 
   end
