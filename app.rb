8ce4d2ce535651479bba16c2f0a1e6b3b4c6d66a
Michael Carroll <carrollm2@gmail.com>
2020-03-03 02:45:28 +0000

    Done.

--- app.rb
+++ app.rb
@@ -13,6 +13,9 @@ module FormsLab
     end
 
     post '/pirates' do
+      @pirate = Pirate.new(params[:pirate_name], params[:pirate_weight], params[:pirate_height])
+      @ship_1 = Ship.new(params[:ship_name_1], params[:ship_type_1], params[:ship_booty_1])
+      @ship_2 = Ship.new(params[:ship_name_2], params[:ship_type_2], params[:ship_booty_2])
       erb :show
     end
 
