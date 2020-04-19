78b42ebf79b022626989b341b3ebd739281e50d3
Chad Lengefeld <chadlengefeld@gmail.com>
2020-04-19 20:38:04 +0000

    Done.

---
+++ app/app.rb
@@ -0,0 +1,8 @@
+require_relative '../environment.rb'
+
+
+class App < Sinatra::Base
+  get '/' do
+  end
+
+end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,20 @@
 class Ship
+attr_reader :name, :type, :booty
+SHIPS = []
+
+def initialize(ship)
+  @name = ship[:name]
+  @type = ship[:type]
+  @booty = ship[:booty]
+end
+
+def self.all
+  SHIPS
+end
+
+def self.clear
+  @@all.destroy
+end
+
+
 end
