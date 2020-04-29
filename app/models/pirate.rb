66e7372cd37065976145f483fcf3bbb34d1f85a2
jkellyphilly <joel.kelly.philly@gmail.com>
2020-04-28 22:36:06 -0400

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,15 @@
 class Pirate
+
+  attr_accessor :name, :weight, :height
+
+  @@all = []
+
+  def initialize
+    self.class.all << self
+  end
+
+  def self.all
+    @@all
+  end
+  
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,19 @@
 class Ship
+
+  attr_accessor :name, :type, :booty
+
+  @@all = []
+
+  def initialize
+    self.class.all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.clear
+    self.all.clear
+  end
+  
 end
