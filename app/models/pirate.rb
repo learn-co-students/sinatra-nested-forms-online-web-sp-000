1c9a31dbdb3d8138eb2e9a15e25e1a273cf582af
Taishawnk <Taishawnking1@yahoo.com>
2020-06-05 12:00:01 -0600

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,14 @@
 class Pirate
+    attr_reader :name, :weight, :height
+    @@all=[]
+    def initilize(name, weight, height)
+        @name=params[:name]
+        @weight=params[:weight]
+        @height=params[:height]
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
 end
