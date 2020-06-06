42dd3c133b38819bec466ad80483adf66d382b8a
Taishawnk <Taishawnking1@yahoo.com>
2020-06-06 12:41:22 -0600

    Done.

--- app.rb
+++ app.rb
@@ -13,12 +13,10 @@ module FormsLab
 
      post "/pirates" do
       @pirate = Pirate.new(params[:pirate])
-
       params[:pirate][:ships].each do |details|
         Ship.new(details)
       end
       @ships = Ship.all
-
       erb :'pirates/show'
     end
   end	
--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,7 +1,7 @@
 class Pirate
     attr_reader :name, :weight, :height
     @@all=[]
-    def initilize(name, weight, height)
+    def initialize(name, weight, height)
         @name=params[:name]
         @weight=params[:weight]
         @height=params[:height]
