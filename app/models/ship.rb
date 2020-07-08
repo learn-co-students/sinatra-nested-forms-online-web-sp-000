eb0f667c1e21f5519a47f4dbafad94a200ba7cf2
ting682 <tchung682@gmail.com>
2020-07-08 09:32:12 -0400

    Done.

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,23 @@
 class Ship
+    attr_reader :name, :type, :booty
+    @@all = []
+
+    def initialize (name, type, booty)
+        @name = name
+        @type = type
+        @booty = booty
+        save
+    end
+
+    def self.all
+        @@all
+    end
+
+    def save
+        @@all << self
+    end
+
+    def self.clear
+        @@all = []
+    end
 end
