6fa375fb411028853a491176ceafb3b1fc30dc3d
ismayton <ian.mayton@gmail.com>
2020-08-31 13:35:06 -0500

    Done.

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,25 @@
 class Ship
+    attr_accessor :name, :type, :booty
+
+    @@all = []
+
+    def initialize(params)
+        @name = params[:name]
+        @type = params[:type]
+        @booty = params[:booty]
+        self.save
+    end 
+
+    def save
+        @@all << self 
+    end 
+
+    def self.all 
+        @@all 
+    end 
+
+    def self.clear
+        @@all.clear 
+    end 
+    
 end
