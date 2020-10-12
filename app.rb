2f1eba3f56cfae39f63c32061d40600faf995e09
javonesm89 <javosmith12@gmail.com>
2020-10-12 10:23:12 -0400

    Done.

--- app.rb
+++ app.rb
@@ -2,8 +2,24 @@ require './environment'
 
 module FormsLab
   class App < Sinatra::Base
+    
+    get '/' do
+      erb :root
+    end
 
-    # code other routes/actions here
+    get '/new' do
+      erb :'pirates/new'
+    end
 
+    post '/pirates' do
+      params.inspect
+      # erb :'pirates/new'
+    end
+  
+  
+  
+  
+  
+  
   end
 end
