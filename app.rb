22734129a65189dc53c98a01945a55c8a3960885
asb1512 <andrew@olympicmedia.com>
2020-11-18 14:38:16 -0500

    Done.

--- app.rb
+++ app.rb
@@ -7,7 +7,17 @@ module FormsLab
         end
 
         get '/new' do
-            erb :new
+            erb :'pirates/new'
+        end
+
+        post '/pirates' do
+            
+            @pirate = Pirate.new(params[:pirate][:name], params[:pirate][:weight], params[:pirate][:height])
+                        
+            array = params[:pirate][:ships].each {|ship| Ship.new(ship[:name], ship[:type], ship[:booty]) }
+            @all_ships = Ship.all
+            # binding.pry
+            erb :'pirates/show'
         end
     end
 end
