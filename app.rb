3c0f41e3a1a3d975660ddbd9a9f7e9005c0d02ef
Benjamin Cheng <bencheng200@gmail.com>
2021-01-06 17:29:11 +0000

    Done.

--- app.rb
+++ app.rb
@@ -4,6 +4,20 @@ module FormsLab
   class App < Sinatra::Base
 
     # code other routes/actions here
+    get '/' do
+      erb :'pirates/new'
+    end
 
+    post '/show' do
+      @pirates = Pirate.new(params[:pirate])
+
+      params[:pirates][:ships].each do |ship|
+        Ship.new(ship.name, ship.type, ship.booty)
+      end
+
+      @ships = Ship.all
+
+      erb :'pirates/show'
+    end
   end
 end
