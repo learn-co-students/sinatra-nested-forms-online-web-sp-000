a6381594dcd1ccdf5b646a770fcb78f718f03717
Christerle Nounoute <christerlesn@gmail.com>
2021-02-07 06:52:11 +0000

    Done.

--- app.rb
+++ app.rb
@@ -3,11 +3,11 @@ require './environment'
 module FormsLab
   class App < Sinatra::Base
     get '/' do
+      "Welcome to the Nested Forms Lab!"
+    end
+    get '/new' do
       erb :new
     end
-
-
-    # code other routes/actions here
 
   end
 end
