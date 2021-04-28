26e93c9c48dac42f2b0bebc475fa76070ccf70b0
ben-altman <60247694+ben-altman@users.noreply.github.com>
2021-04-27 23:49:53 -0400

    Done.

--- app.rb
+++ app.rb
@@ -4,6 +4,44 @@ module FormsLab
   class App < Sinatra::Base
 
     # code other routes/actions here
+    get '/' do
+      erb :root
+    end
+
+    get '/new' do
+      erb :'pirates/new'
+    end
+
+    post '/pirates' do
+      @pirate = Pirate.new(params[:pirate])
+
+      params[:pirate][:ships].each do |details|
+        Ship.new(details)
+      end
+      
+      @ships = Ship.all
+
+      erb :'pirates/show'
+    end
 
   end
 end
+
+
+# module FormsLab
+#   class App < Sinatra::Base
+#     get '/' do 
+#       erb :root 
+#     end
+#     get '/new' do 
+#       erb :'pirates/new'
+#     end
+#     post '/pirates' do 
+#       @pirate = Pirate.new(params[:pirate])
+#       @ship_1 = Ship.new(params[:pirate][:ships][0])
+#       @ship_2 = Ship.new(params[:pirate][:ships][1])
+#       erb :"pirates/show" 
+#     end
+#     # code other routes/actions here
+#   end
+# end
